a1304975e772c441a2105badfdbd39a594eb5216
a4aneel <a4aneel@users.noreply.github.com>
2025-03-10 22:22:06 +0000

    Automated deployment: Mon Mar 10 22:22:06 UTC 2025 f2abe98d16472864b39d30a7547b3d4aba380c51

--- tests/pages/preferred-pom/sauce-demo-mini-cart.ts
+++
@@ -1,8 +0,0 @@
-import { expectElementToHaveText } from '../../../src/anil-playwright/utils/assert-utils';
-import { getLocator } from '../../../src/anil-playwright/utils/locator-utils';
-
-const miniCartCount = () => getLocator(`//*[@id='shopping_cart_container']//span`);
-
-export async function verifyMiniCartCount(expMiniCartCount: string) {
-  await expectElementToHaveText(miniCartCount(), expMiniCartCount);
-}
